ab36f15b7eab3a7ed61e0132f71e7bfc9eea9e90
ramez <ramezhishamahmad@gmail.com>
2024-03-21 18:19:04 +0300

    init commit

---
+++ src/components/Modal.jsx
@@ -0,0 +1,23 @@
+import { forwardRef, useImperativeHandle, useRef, useEffect } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children, open }) => {
+  const dialog = useRef();
+
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
+
+  return createPortal(
+    <dialog className="modal" ref={dialog}>
+      {children}
+    </dialog>,
+    document.getElementById("modal")
+  );
+};
+
+export default Modal;
